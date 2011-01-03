ca98c17f001ed1d6b9c834b05b28002fc1c632c3
Wilker Lucio <wilkerlucio@gmail.com>
2011-01-03 02:26:05 -0300

    method descriptor is verifying return value and arguments

--- lib/aidmock/method_descriptor.rb
+++ lib/aidmock/method_descriptor.rb
@@ -24,13 +24,109 @@ module Aidmock
 
     def initialize(name, type, *arguments)
       @name = name
-      @type = type
-      @arguments = arguments
+      @type = ::Aidmock::Matchers.create(type)
+      @arguments = arguments.map { |arg| ::Aidmock::Matchers.create(arg) }
     end
 
     def verify(double)
-      # TODO check return type
-      # TODO check arguments
+      verify_arguments(double)
+      verify_return(double)
+    end
+
+    def verify_arguments(double)
+      arguments = double.arguments
+      check_method_arity!(double)
+      current_matcher = 0
+
+      while arguments.length > 0
+        matcher = @arguments[current_matcher]
+
+        if matcher.instance_of? ::Aidmock::Matchers::SplatArgMatcher
+          arg = arguments
+          arguments = []
+        else
+          arg = arguments.shift
+        end
+
+        unless matcher.match?(arg)
+          raise Aidmock::MethodInterfaceArgumentsNotMatchError.new("argument value #{arg.inspect} doesn't match with #{matcher.inspect}")
+        end
+
+        current_matcher += 1
+      end
+    end
+
+    def verify_return(value)
+      unless @type.match?(value)
+        raise Aidmock::MethodInterfaceReturnNotMatchError.new("Return value #{value.inspect} doesn't match with #{@type.inspect}")
+      end
+    end
+
+    def arity
+      @arity ||= begin
+        arity = 0
+
+        @arguments.each do |arg|
+          arity += 1
+
+          if optional_arg? arg
+            arity *= -1
+            return arity
+          end
+        end
+
+        arity
+      end
+    end
+
+    def required_arity
+      @required_arity = begin
+        required = arity
+
+        if required < 0
+          required *= -1
+          required -= 1
+        end
+
+        required
+      end
+    end
+
+    def max_number_of_arguments
+      return nil if has_a_splat?
+
+      @arguments.length
+    end
+
+    protected
+
+    def check_method_arity!(double)
+      length = double.arguments.length
+      # in case of positive arity (fixed number of arguments)
+      if arity >= 0
+        if length != arity
+          raise Aidmock::MethodInterfaceArgumentsNotMatchError.new("error on mock method #{double.method.inspect}, expected #{arity} arguments, #{length} sent")
+        end
+      # on negative arity (variable number of arguments)
+      else
+        if length < required_arity
+          raise Aidmock::MethodInterfaceArgumentsNotMatchError.new("error on mock method #{double.method.inspect}, expected at least #{required_arity}, #{length} sent")
+        end
+
+        if max = max_number_of_arguments
+          if length > max
+            raise Aidmock::MethodInterfaceArgumentsNotMatchError.new("error on mock method #{double.method.inspect}, expected at most #{max}, #{length} sent")
+          end
+        end
+      end
+    end
+
+    def has_a_splat?
+      @arguments.last.instance_of? ::Aidmock::Matchers::SplatArgMatcher
+    end
+
+    def optional_arg?(arg)
+      arg.instance_of? Matchers::OptionalArgMatcher or arg.instance_of? Matchers::SplatArgMatcher
     end
   end
 end
--- spec/aidmock/method_descriptor_spec.rb
+++ spec/aidmock/method_descriptor_spec.rb
@@ -19,7 +19,179 @@
 # THE SOFTWARE.
 
 describe Aidmock::MethodDescriptor do
-  it "initiaze with name, type and arguments"
-  it "verify the return value"
-  it "verify the arguments"
+  MockDescriptor = Aidmock::Frameworks::MockDescriptor
+  md = Aidmock::MethodDescriptor
+  m = Aidmock::Matchers
+
+  trueMatch = Class.new do
+    def match?(obj)
+      true
+    end
+  end.new
+
+  falseMatch = Class.new do
+    def match?(obj)
+      false
+    end
+  end.new
+
+  def mock_return(val)
+    MockDescriptor.new(nil, :some, val)
+  end
+
+  def mock_args(args)
+    MockDescriptor.new(nil, :some, nil, args)
+  end
+
+  it "verify the arguments and return value" do
+    double = MockDescriptor.new(nil, :method, :result, [:args])
+    desc = md.new(:some, nil)
+    desc.should_receive(:verify_arguments).with(double)
+    desc.should_receive(:verify_return).with(double)
+    desc.verify(double)
+  end
+
+  context "verifying return value" do
+    it "raise error if return value don't match" do
+      desc = md.new(:some, falseMatch)
+
+      expect { desc.verify_return(mock_return(nil)) }.to raise_error(Aidmock::MethodInterfaceReturnNotMatchError)
+    end
+
+    it "not raise error if return value match" do
+      desc = md.new(:some, trueMatch)
+
+      expect { desc.verify_return(mock_return(nil)) }.to_not raise_error
+    end
+  end
+
+  context "verifying arguments" do
+    context "testing arguments arity" do
+      context "on fixed arguments length" do
+        it "raise error if user send less arguments than method expects" do
+          desc = md.new(:some, nil, trueMatch, trueMatch)
+
+          expect { desc.verify_arguments(mock_args([5]))}.to raise_error(Aidmock::MethodInterfaceArgumentsNotMatchError, "error on mock method :some, expected 2 arguments, 1 sent")
+        end
+
+        it "raise error if user send more arguments than method expects" do
+          desc = md.new(:some, nil, trueMatch, trueMatch)
+
+          expect { desc.verify_arguments(mock_args([5, 6, 7]))}.to raise_error(Aidmock::MethodInterfaceArgumentsNotMatchError, "error on mock method :some, expected 2 arguments, 3 sent")
+        end
+
+        it "not raise error if they are in correct arity" do
+          desc = md.new(:some, nil, trueMatch, trueMatch)
+
+          expect { desc.verify_arguments(mock_args([5, 6]))}.to_not raise_error
+        end
+      end
+
+      context "on variable arguments length" do
+        it "raise error if number of arguments is less than required ones" do
+          desc = md.new(:some, nil, trueMatch, Aidmock::Matchers::OptionalArgMatcher.new(trueMatch))
+
+          expect { desc.verify_arguments(mock_args([])) }.to raise_error(Aidmock::MethodInterfaceArgumentsNotMatchError, "error on mock method :some, expected at least 1, 0 sent")
+        end
+
+        it "raise error if it has more args than possible (after optional ones)" do
+          desc = md.new(:some, nil, trueMatch, Aidmock::Matchers::OptionalArgMatcher.new(trueMatch))
+
+          expect { desc.verify_arguments(mock_args([1, 2, 3])) }.to raise_error(Aidmock::MethodInterfaceArgumentsNotMatchError, "error on mock method :some, expected at most 2, 3 sent")
+        end
+
+        it "dont raise error if fits on argument length" do
+          desc = md.new(:some, nil, trueMatch, Aidmock::Matchers::OptionalArgMatcher.new(trueMatch))
+
+          expect { desc.verify_arguments(mock_args([1])) }.to_not raise_error
+          expect { desc.verify_arguments(mock_args([1, 2])) }.to_not raise_error
+        end
+
+        it "dont raise error if has more arguments than max, but has a splat" do
+          desc = md.new(:some, nil, trueMatch, Aidmock::Matchers::SplatArgMatcher.new(trueMatch))
+
+          expect { desc.verify_arguments(mock_args([1, 2, 3])) }.to_not raise_error
+        end
+      end
+    end
+
+    context "checking arguments" do
+      it "try each argument on respective matcher" do
+        m1 = mock
+        m2 = mock
+
+        m1.should_receive(:match?).with(1).and_return(true)
+        m2.should_receive(:match?).with(2).and_return(true)
+
+        desc = md.new(:some, nil, m1, m2)
+        desc.verify_arguments(mock_args([1, 2]))
+      end
+
+      it "raise error if any matcher fail" do
+        desc = md.new(:some, nil, falseMatch, trueMatch)
+
+        expect { desc.verify_arguments(mock_args([1, 2])) }.to raise_error(Aidmock::MethodInterfaceArgumentsNotMatchError)
+      end
+
+      it "send an array with others if has a splat" do
+        matcher = mock
+        matcher.should_receive(:match?).with(2).and_return(true)
+        matcher.should_receive(:match?).with(3).and_return(true)
+
+        desc = md.new(:some, nil, trueMatch, Aidmock::Matchers::SplatArgMatcher.new(matcher))
+        desc.verify_arguments(mock_args([1, 2, 3]))
+      end
+    end
+  end
+
+  context "#arity" do
+    it "return 0 if no arguments are given" do
+      md.new(:some, nil).arity.should == 0
+    end
+
+    it "return positive number of arguments if they are regular ones" do
+      md.new(:some, nil, nil, nil).arity.should == 2
+    end
+
+    it "return -1 if start with optional argument" do
+      md.new(:some, nil, m::OptionalArgMatcher.new(nil)).arity.should == -1
+    end
+
+    it "return -1 if start with splat argument" do
+      md.new(:some, nil, m::SplatArgMatcher.new(nil)).arity.should == -1
+    end
+
+    it "return negative value when has both optional and required args" do
+      md.new(:some, nil, nil, m::OptionalArgMatcher.new(nil)).arity.should == -2
+    end
+
+    it "return negative value when has both splt and required args" do
+      md.new(:some, nil, nil, m::SplatArgMatcher.new(nil)).arity.should == -2
+    end
+
+    it "return negative when have optional, required and splt args" do
+      md.new(:some, nil, nil, m::OptionalArgMatcher.new(nil), m::SplatArgMatcher.new(nil)).arity.should == -2
+    end
+  end
+
+  context "#required_arity" do
+    before :each do
+      @m = md.new(:some, nil)
+    end
+
+    it "return arity if its positive" do
+      @m.stub(:arity).and_return(1)
+      @m.required_arity.should == 1
+    end
+
+    it "return 0 if arity is 0" do
+      @m.stub(:arity).and_return(0)
+      @m.required_arity.should == 0
+    end
+
+    it "return arity positive less 1 if it's negative" do
+      @m.stub(:arity).and_return(-2)
+      @m.required_arity.should == 1
+    end
+  end
 end
