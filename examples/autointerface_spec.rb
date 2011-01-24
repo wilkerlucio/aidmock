74a5f92ece0c77982295a5d616e5951ee3934c60
Wilker Lucio <wilkerlucio@gmail.com>
2011-01-24 11:03:36 -0300

    example for autointerface

---
+++ examples/autointerface_spec.rb
@@ -0,0 +1,42 @@
+$: << File.expand_path("../../lib", __FILE__)
+require 'aidmock'
+
+RSpec.configure do |config|
+  config.after :each do
+    Aidmock.verify
+  end
+end
+
+class Person
+  def self.something(hi)
+
+  end
+
+  def first_name
+    "first"
+  end
+
+  def last_name
+    "last"
+  end
+
+  def full_name
+    first_name + " " + last_name
+  end
+end
+
+Aidmock::Sanity.sanitize
+
+describe Person do
+  before :each do
+    @person = Person.new
+  end
+
+  context ".first_name" do
+    it "should return some" do
+      @person.stub(:first_name).and_return("f")
+      @person.stub(:last_name).and_return("l")
+      @person.full_name.should == "f l"
+    end
+  end
+end
